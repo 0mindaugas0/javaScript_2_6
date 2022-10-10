8fff8db2ac502d7934d149d9c52c87266d2ba3fa
0mindaugas0 <mindaugas.mizaras@gmail.com>
2022-10-10 08:18:14 +0300

    Sukurtas projektas

---
+++ script.js
@@ -0,0 +1,12 @@
+let a = 1;
+
+let faktorialas1 = faktorialas(a);
+
+function faktorialas(skaicius){
+    console.log (skaicius * 1);
+    console.log (skaicius * 1 * 2);
+    console.log (skaicius * 1 * 2 * 3);
+    console.log (skaicius * 1 * 2 * 3 * 4);
+    console.log (skaicius * 1 * 2 * 3 * 4 * 5);
+}
+
